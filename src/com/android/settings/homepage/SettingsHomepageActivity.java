5463053dc2b243ff6d6a2c62a31d3dc574aa3242
Lucchetto <lucchetto.tie@live.com>
2019-12-28 13:49:56 +0100

    Settings: blur the search bar
    Change-Id: I60eafbec999a03402d190c8c820e82974ff1ed1f
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -28,6 +28,7 @@ import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toolbar;
@@ -45,8 +46,9 @@ import com.android.settings.accounts.AvatarViewMixin;
 import com.android.settings.core.HideNonSystemOverlayMixin;
 import com.android.settings.homepage.contextualcards.ContextualCardsFragment;
 import com.android.settings.overlay.FeatureFactory;
-
 import com.android.settingslib.drawable.CircleFramedDrawable;
+import eightbitlab.com.blurview.BlurView;
+import eightbitlab.com.blurview.RenderScriptBlur;
 
 public class SettingsHomepageActivity extends FragmentActivity {
 
@@ -59,7 +61,8 @@ public class SettingsHomepageActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.settings_homepage_container);
-        final View root = findViewById(R.id.settings_homepage_container);
+        final View decorView = getWindow().getDecorView();
+        final ViewGroup root = (ViewGroup) decorView.findViewById(android.R.id.content);
         root.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
@@ -68,7 +71,10 @@ public class SettingsHomepageActivity extends FragmentActivity {
         Context context = getApplicationContext();
 
         mUserManager = context.getSystemService(UserManager.class);
-
+        final BlurView searchBarBlur = findViewById(R.id.search_bar_blur);
+        final Drawable windowBackground = decorView.getBackground();
+        searchBarBlur.setupWith(root).setFrameClearDrawable(windowBackground)
+                .setBlurAlgorithm(new RenderScriptBlur(this));
         final Toolbar toolbar = findViewById(R.id.search_action_bar);
         FeatureFactory.getFactory(this).getSearchFeatureProvider()
                 .initSearchToolbar(this /* activity */, toolbar, SettingsEnums.SETTINGS_HOMEPAGE);
